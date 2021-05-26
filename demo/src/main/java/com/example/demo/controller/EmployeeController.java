23a89fb052db3e8b079a683d6ceed8e54cfa4103
shafeelsinan <mohammedshafeelsinan@gmail.com>
2021-05-26 19:50:52 +0530

    second messge

--- demo/src/main/java/com/example/demo/controller/EmployeeController.java
+++ demo/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,12 @@ public class EmployeeController {
 	public List<Employee> getAllEmp()
 	{
 		return repository.findAll();
+	}
+	
+	@PostMapping("/employees")
+	public Employee createEmployeeloyee(Employee emp)
+	{
+		return repository.save(emp);
 	}
 
 }
